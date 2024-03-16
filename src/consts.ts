ab74e6c34cc3dfd9ec231f70673049eeab36f773
sulinehk <sulinehk@gmail.com>
2024-03-16 16:55:59 +0800

    feat: add title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'sulinehk blog';
+export const SITE_DESCRIPTION = 'sulinehk blog 是一个致力于提供高质量技术文章的个人博客，内容囊括整个计算机科学学科，具体讨论的主题有软件工程、数据结构与算法分析和 Go 语言实践等。';
