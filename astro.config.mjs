253137459006ea6629a586e34e92aca36c28f48b
sulinehk <sulinehk@gmail.com>
2024-03-18 14:53:47 +0800

    feat: split AUTHOR and TITLE

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import {defineConfig} from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
   integrations: [sitemap()],
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import rss from '@astrojs/rss';
 import {getCollection} from 'astro:content';
-import {SITE_TITLE, SITE_DESCRIPTION} from '@consts';
+import {AUTHOR, DESCRIPTION} from '@consts';
 
 export async function GET(context) {
   const posts = await getCollection('post');
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: AUTHOR,
+    description: DESCRIPTION,
     site: context.site,
     items: posts.map((p) => ({
       ...p.data,
