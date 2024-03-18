0c21d1fdae608efa1448eb0810e2a721361421b5
sulinehk <sulinehk@gmail.com>
2024-03-18 19:50:49 +0800

    feat: use tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
-  integrations: [sitemap()],
-  compressHTML: false,
+  integrations: [sitemap(), tailwind()],
+  compressHTML: false
 });
