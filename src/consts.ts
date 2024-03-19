41ef6946831b26b949e4c14954c16c226f92ce0d
sulinehk <sulinehk@gmail.com>
2024-03-19 20:20:34 +0800

    feat: add post count

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const AUTHOR = 'sulinehk';
 export const TITLE = 'sulinehk blog - 专注于计算机科学与软件工程的技术博客'
 export const DESCRIPTION = 'sulinehk blog 是一个致力于提供高质量技术文章的个人博客，内容囊括整个计算机科学学科，具体讨论的主题有软件工程、数据结构与算法分析和 Go 语言实践等。';
+export const SINCE = '2017';
