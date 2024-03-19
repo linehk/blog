43b4883b9875b43a854eb7be51ccf7ffd12a77be
sulinehk <sulinehk@gmail.com>
2024-03-19 14:39:15 +0800

    refactor: use tailwind rewrite css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,10 @@ export default {
     extend: {},
   },
   plugins: [
-    require("daisyui"),
     require('@tailwindcss/typography'),
+    require("daisyui"),
   ],
+  daisyui: {
+    themes: ["light"],
+  },
 }
