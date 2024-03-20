a005f35f9523815b2ef323dab9b180bf6810d2e9
sulinehk <sulinehk@gmail.com>
2024-03-20 14:10:48 +0800

    feat: compress HTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
@@ -7,6 +7,5 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
-  integrations: [sitemap(), tailwind(), robotsTxt()],
-  compressHTML: false
+  integrations: [sitemap(), tailwind(), robotsTxt()]
 });
