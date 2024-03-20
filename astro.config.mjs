9071dd757a338246cd6b770688f2b1fd81a4533f
sulinehk <sulinehk@gmail.com>
2024-03-20 16:27:16 +0800

    feat: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
-  integrations: [sitemap(), tailwind(), robotsTxt()]
+  integrations: [sitemap(), tailwind(), robotsTxt(), partytown()]
 });
