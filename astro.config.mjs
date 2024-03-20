6b8329b1dbe3b491f841a7fbfbdd008f521d3631
sulinehk <sulinehk@gmail.com>
2024-03-20 18:03:50 +0800

    fix: CORS problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
@@ -8,5 +8,14 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
-  integrations: [sitemap(), tailwind(), robotsTxt(), partytown()]
+  integrations: [sitemap(), tailwind(), robotsTxt(), partytown()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name]-[hash].js',
+        },
+      },
+    },
+  },
 });
