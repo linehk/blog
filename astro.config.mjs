b9b758a5f9a358b5c4421c47f43212c5f9596c43
sulinehk <sulinehk@gmail.com>
2024-03-20 14:03:34 +0800

    feat: add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), robotsTxt()],
   compressHTML: false
 });
