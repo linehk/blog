fac5e4dc5cfcfb84d58fb2bc31bca34d4c1fd892
sulinehk <sulinehk@gmail.com>
2024-03-21 12:09:04 +0800

    feat: add read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
+import {remarkReadingTime} from './remark-reading-time.mjs';
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sulinehk.com',
   integrations: [sitemap(), tailwind(), robotsTxt(), partytown()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   vite: {
     build: {
       rollupOptions: {
