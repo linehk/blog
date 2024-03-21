e98e6b840976510b0e290b7f2314a543ce041650
sulinehk <sulinehk@gmail.com>
2024-03-21 11:51:36 +0800

    feat: add rss content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,24 @@
 import rss from '@astrojs/rss';
 import {getCollection} from 'astro:content';
 import {AUTHOR, DESCRIPTION} from '@consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const posts = await getCollection('post');
+  const posts = (await getCollection('post')).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
   return rss({
+    stylesheet: '/pretty-feed-v3.xsl',
     title: AUTHOR,
     description: DESCRIPTION,
     site: context.site,
     items: posts.map((p) => ({
       ...p.data,
       link: `/post/${p.slug}/`,
+      content: sanitizeHtml(parser.render(p.body)),
     })),
   });
 }
